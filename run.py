f60d3924a6db77f4bd06732bf5e63a8b50fd07b3
FRNBDN <francesco.badini94@gmail.com>
2023-07-04 06:01:06 +0000

    add card examples

---
+++ run.py
@@ -0,0 +1,38 @@
+cards = [
+    {
+        "Owner": "John Doe",
+        "Number": "1234567812345678",
+        "PIN": "1234",
+        "Accounts": [
+            {"Name": "Checking", "Balance": 500},
+            {"Name": "Savings", "Balance": 10000},
+        ],
+    },
+    {
+        "Owner": "Jane Smith",
+        "Number": "9876543210987654",
+        "PIN": "4321",
+        "Accounts": [
+            {"Name": "Checking", "Balance": 1200},
+            {"Name": "Savings", "Balance": 15000},
+        ],
+    },
+    {
+        "Owner": "Sarah Johnson",
+        "Number": "5678901256789012",
+        "PIN": "2468",
+        "Accounts": [
+            {"Name": "Checking", "Balance": 800},
+            {"Name": "Savings", "Balance": 5000},
+        ],
+    },
+    {
+        "Owner": "Michael Brown",
+        "Number": "3456789034567890",
+        "PIN": "9876",
+        "Accounts": [
+            {"Name": "Checking", "Balance": 1500},
+            {"Name": "Savings", "Balance": 20000},
+        ],
+    },
+]
